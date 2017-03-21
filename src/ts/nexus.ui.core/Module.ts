7dd23c21da147f1bb994127c7cab3af4bd7a2c5a
Axel Helmert <axel.helmert@luka.de>
2017-03-21 16:54:21 +0100

    Migrate to TypeScript

--- src/ts/nexus.ui.core/Module.ts
+++ src/ts/nexus.ui.core/Module.ts
@@ -3,10 +3,13 @@ import 'angular-material';
 import 'angular-resource';
 
 import * as auth from './Services/AuthService';
+import * as repos from './Services/Repository';
 
 const MODULE_NAME: string = 'nexus.ui.core';
 let UiCoreModule = ng.module(MODULE_NAME, [ 'ngMaterial', 'ngResource']);
-UiCoreModule.service(auth.SERVICE_NAME, auth.OAuthService);
+
+UiCoreModule.service(auth.SERVICE_NAME, auth.OAuthService)
+    .factory(repos.API_SERVICE_NAME, function() {});
 
 export {UiCoreModule};
 export default MODULE_NAME;
--- src/ts/nexus.ui.core/Services/Repository.ts
+++ src/ts/nexus.ui.core/Services/Repository.ts
@@ -3,7 +3,9 @@
  */
 
 import * as ng from 'angular';
-import 'angular-resource';
+import * as entities from '../Entities';
+
+export const API_SERVICE_NAME : string = 'nexus.deployment.api';
 
 /** Defines the result set */
 export interface ResultSet<Entity>
@@ -16,21 +18,42 @@ export interface ResultSet<Entity>
 export interface IRepository<Entity>
 {
     findOne(id: string|number): ng.IPromise<Entity>;
-    find(query?: {[key: string]: string}): ng.IPromise<ResultSet<Entity>>;
+    find(query?: {[key: string]: string}): ng.IPromise<ResultSet<Entity>>;   
+}
+
+export interface IUpdatableRepository<Entity> {
     save(entity: Entity): ng.IPromise<Entity>;
+}
+
+export interface IRemovableRepository<Entity> {
     remove(entity: Entity): ng.IPromise<Entity>;
 }
 
-/**
- * Implements the REST repository 
- */
-export class RestRepository<Entity>
+export interface IFullEntityRepository<Entity>
 {
-    protected http: ng.IHttpService;
-
-    /** Constructor*/
-    construct(http: ng.IHttpService)
-    {
-        this.http = http;
-    }
 }
+
+/** Defines the application repo */
+export interface IApplicationRepository extends IRepository<entities.Application>, IUpdatableRepository<entities.Application>
+{}
+
+/** Package repository definition */
+export interface IPackageRepository extends IRepository<entities.Package>
+{
+    upload(file: object) : ng.IPromise<entities.Package>;
+}
+
+/** Repo definition for deploy targets */
+export interface IDeployTargetRepository extends IFullEntityRepository<entities.DeployTarget>
+{
+    deploy(target: entities.DeployTarget, pkg: entities.ApplicationInstance) : ng.IPromise<entities.ApplicationInstance>;
+}
+
+/** Defines the deployment API */
+export interface IDeploymentApi
+{
+    applications: IApplicationRepository;
+    packages: IPackageRepository;
+    deployTargets: IDeployTargetRepository;
+}
+
